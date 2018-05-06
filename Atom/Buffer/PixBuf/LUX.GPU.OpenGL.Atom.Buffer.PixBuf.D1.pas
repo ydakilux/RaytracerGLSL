4783881cb58bd479d7429def1b723f2ee2dcd951
luxidea <contact@luxidea.net>
2018-05-07 02:51:16 +0900

    TGLPoiPix*D / TGLCelPix*D クラスを追加。

--- Atom/Buffer/PixBuf/LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D1.pas
+++ Atom/Buffer/PixBuf/LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D1.pas
@@ -21,7 +21,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPixBuf1D<_TItem_>
 
      IGLPixBuf1D = interface( IGLBuffer )
-     ['{335CA6ED-CF5C-4145-B561-4F1AC430C9DD}']
+     ['{441F1A9D-EC3A-43DE-94D6-467ACF6AA0A8}']
      {protected}
        ///// アクセス
        function GetItemsX :Integer;
@@ -45,6 +45,70 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      public
        ///// プロパティ
        property ItemsX :Integer read GetItemsX write SetItemsX;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPoiPix1D<_TItem_>
+
+     IGLPoiPix1D = interface( IGLPixBuf1D )
+     ['{E9A6BD3C-D3F2-461E-8EF6-2224AB77E315}']
+     {protected}
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+     {public}
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TGLPoiPix1D<_TItem_:record> = class( TGLPixBuf1D<_TItem_>, IGLPoiPix1D )
+     private
+     protected
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+     public
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCelPix1D<_TItem_>
+
+     IGLCelPix1D = interface( IGLPixBuf1D )
+     ['{89BBCE48-2D21-44D0-AC4D-B4ED6110B674}']
+     {protected}
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+     {public}
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TGLCelPix1D<_TItem_:record> = class( TGLPixBuf1D<_TItem_>, IGLCelPix1D )
+     private
+     protected
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+     public
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property CellsX :Integer read GetCellsX write SetCellsX;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -82,6 +146,70 @@ end;
 procedure TGLPixBuf1D<_TItem_>.MakeBuffer;
 begin
      Count := _ItemsX;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPoiPix1D<_TItem_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLPoiPix1D<_TItem_>.GetPoinsX :Integer;
+begin
+     Result := ItemsX;
+end;
+
+procedure TGLPoiPix1D<_TItem_>.SetPoinsX( const PoinsX_:Integer );
+begin
+     ItemsX := PoinsX_;
+end;
+
+//------------------------------------------------------------------------------
+
+function TGLPoiPix1D<_TItem_>.GetCellsX :Integer;
+begin
+     Result := ItemsX - 1;
+end;
+
+procedure TGLPoiPix1D<_TItem_>.SetCellsX( const CellsX_:Integer );
+begin
+     ItemsX := CellsX_ + 1;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCelPix1D<_TItem_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLCelPix1D<_TItem_>.GetPoinsX :Integer;
+begin
+     Result := ItemsX + 1;
+end;
+
+procedure TGLCelPix1D<_TItem_>.SetPoinsX( const PoinsX_:Integer );
+begin
+     ItemsX := PoinsX_ - 1;
+end;
+
+//------------------------------------------------------------------------------
+
+function TGLCelPix1D<_TItem_>.GetCellsX :Integer;
+begin
+     Result := ItemsX;
+end;
+
+procedure TGLCelPix1D<_TItem_>.SetCellsX( const CellsX_:Integer );
+begin
+     ItemsX := CellsX_;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
--- Atom/Buffer/PixBuf/LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D2.pas
+++ Atom/Buffer/PixBuf/LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D2.pas
@@ -22,7 +22,7 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPixBuf2D<_TItem_>
 
      IGLPixBuf2D = interface( IGLPixBuf1D )
-     ['{A3500166-874F-4CA2-84A8-8DDAFEA56712}']
+     ['{127179DF-CB40-4505-8805-59C0E3A6E411}']
      {protected}
        ///// アクセス
        function GetItemsY :Integer;
@@ -46,6 +46,94 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
      public
        ///// プロパティ
        property ItemsY :Integer read GetItemsY write SetItemsY;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPoiPix2D<_TItem_>
+
+     IGLPoiPix2D = interface( IGLPixBuf2D )
+     ['{2BEE5402-3221-44CF-9278-5D489B0CAC4A}']
+     {protected}
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetPoinsY :Integer;
+       procedure SetPoinsY( const PoinsY_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+       function GetCellsY :Integer;
+       procedure SetCellsY( const CellsY_:Integer );
+     {public}
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property PoinsY :Integer read GetPoinsX write SetPoinsY;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+       property CellsY :Integer read GetCellsX write SetCellsY;
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TGLPoiPix2D<_TItem_:record> = class( TGLPixBuf2D<_TItem_>, IGLPoiPix2D )
+     private
+     protected
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetPoinsY :Integer;
+       procedure SetPoinsY( const PoinsY_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+       function GetCellsY :Integer;
+       procedure SetCellsY( const CellsY_:Integer );
+     public
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property PoinsY :Integer read GetPoinsX write SetPoinsY;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+       property CellsY :Integer read GetCellsX write SetCellsY;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCelPix2D<_TItem_>
+
+     IGLCelPix2D = interface( IGLPixBuf2D )
+     ['{D44A1241-907E-4167-8E1D-F2A473767526}']
+     {protected}
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetPoinsY :Integer;
+       procedure SetPoinsY( const PoinsY_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+       function GetCellsY :Integer;
+       procedure SetCellsY( const CellsY_:Integer );
+     {public}
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property PoinsY :Integer read GetPoinsX write SetPoinsY;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+       property CellsY :Integer read GetCellsX write SetCellsY;
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TGLCelPix2D<_TItem_:record> = class( TGLPixBuf2D<_TItem_>, IGLCelPix2D )
+     private
+     protected
+       ///// アクセス
+       function GetPoinsX :Integer;
+       procedure SetPoinsX( const PoinsX_:Integer );
+       function GetPoinsY :Integer;
+       procedure SetPoinsY( const PoinsY_:Integer );
+       function GetCellsX :Integer;
+       procedure SetCellsX( const CellsX_:Integer );
+       function GetCellsY :Integer;
+       procedure SetCellsY( const CellsY_:Integer );
+     public
+       ///// プロパティ
+       property PoinsX :Integer read GetPoinsX write SetPoinsX;
+       property PoinsY :Integer read GetPoinsX write SetPoinsY;
+       property CellsX :Integer read GetCellsX write SetCellsX;
+       property CellsY :Integer read GetCellsX write SetCellsY;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
@@ -87,7 +175,109 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-/////////////////////////////////////////////////////////////////////// メソッド
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLPoiPix2D<_TItem_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLPoiPix2D<_TItem_>.GetPoinsX :Integer;
+begin
+     Result := ItemsX;
+end;
+
+procedure TGLPoiPix2D<_TItem_>.SetPoinsX( const PoinsX_:Integer );
+begin
+     ItemsX := PoinsX_;
+end;
+
+function TGLPoiPix2D<_TItem_>.GetPoinsY :Integer;
+begin
+     Result := ItemsY;
+end;
+
+procedure TGLPoiPix2D<_TItem_>.SetPoinsY( const PoinsY_:Integer );
+begin
+     ItemsY := PoinsY_;
+end;
+
+//------------------------------------------------------------------------------
+
+function TGLPoiPix2D<_TItem_>.GetCellsX :Integer;
+begin
+     Result := ItemsX - 1;
+end;
+
+procedure TGLPoiPix2D<_TItem_>.SetCellsX( const CellsX_:Integer );
+begin
+     ItemsX := CellsX_ + 1;
+end;
+
+function TGLPoiPix2D<_TItem_>.GetCellsY :Integer;
+begin
+     Result := ItemsY - 1;
+end;
+
+procedure TGLPoiPix2D<_TItem_>.SetCellsY( const CellsY_:Integer );
+begin
+     ItemsY := CellsY_ + 1;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TGLCelPix2D<_TItem_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TGLCelPix2D<_TItem_>.GetPoinsX :Integer;
+begin
+     Result := ItemsX + 1;
+end;
+
+procedure TGLCelPix2D<_TItem_>.SetPoinsX( const PoinsX_:Integer );
+begin
+     ItemsX := PoinsX_ - 1;
+end;
+
+function TGLCelPix2D<_TItem_>.GetPoinsY :Integer;
+begin
+     Result := ItemsY + 1;
+end;
+
+procedure TGLCelPix2D<_TItem_>.SetPoinsY( const PoinsY_:Integer );
+begin
+     ItemsY := PoinsY_ - 1;
+end;
+
+//------------------------------------------------------------------------------
+
+function TGLCelPix2D<_TItem_>.GetCellsX :Integer;
+begin
+     Result := ItemsX;
+end;
+
+procedure TGLCelPix2D<_TItem_>.SetCellsX( const CellsX_:Integer );
+begin
+     ItemsX := CellsX_;
+end;
+
+function TGLCelPix2D<_TItem_>.GetCellsY :Integer;
+begin
+     Result := ItemsY;
+end;
+
+procedure TGLCelPix2D<_TItem_>.SetCellsY( const CellsY_:Integer );
+begin
+     ItemsY := CellsY_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
 
