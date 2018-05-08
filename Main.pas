dfb4166dcf4d1043cf64c210d76a3cae8e341516
luxidea <contact@luxidea.net>
2018-05-08 14:40:14 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,144 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects,
+  Winapi.OpenGL, Winapi.OpenGLext,
+  LUX, LUX.D1, LUX.D2, LUX.D3, LUX.M4, LUX.FMX.Controls,
+  LUX.GPU.OpenGL,
+  LUX.GPU.OpenGL.Atom.Buffer.StoBuf,
+  LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D1,
+  LUX.GPU.OpenGL.Atom.Buffer.PixBuf.D2,
+  LUX.GPU.OpenGL.Atom.Imager,
+  LUX.GPU.OpenGL.Atom.Imager.D1.Preset,
+  LUX.GPU.OpenGL.Atom.Imager.D2.Preset,
+  LUX.GPU.OpenGL.Atom.Textur.D1.Preset,
+  LUX.GPU.OpenGL.Atom.Textur.D2.Preset,
+  LUX.GPU.OpenGL.Comput;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+    _ImageW :Integer;
+    _ImageH :Integer;
+    _Comput :TGLComput;
+    _Buffer :TGLStoBuf<TSingleM4>;
+    _Textur :TGLPoiTex1D_TAlphaColorF;
+    _Imager :TGLCelIma2D_TAlphaColorF;
+    ///// メソッド
+    procedure InitComput;
+    procedure InitBuffer;
+    procedure InitTextur;
+    procedure InitImager;
+    procedure Draw;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+uses System.Math;
+
+{$R *.fmx}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TForm1.InitComput;
+begin
+     _Comput.ItemsX := 10;
+     _Comput.ItemsY := 10;
+     _Comput.ItemsZ :=  1;
+
+     _Comput.WorksX := _ImageW;
+     _Comput.WorksY := _ImageH;
+     _Comput.WorksZ :=       1;
+
+     _Comput.ShaderC.Source.LoadFromFile( '..\..\_DATA\Comput.glsl' );
+
+     Assert( _Comput.ShaderC.Status, _Comput.ShaderC.Errors.Text );
+     Assert( _Comput.Engine .Status, _Comput.Engine .Errors.Text );
+
+     _Comput.Buffers.Add( 'TBuffer', _Buffer );
+     _Comput.Imagers.Add( '_Imager', _Imager );
+     _Comput.Texturs.Add( '_Textur', _Textur );
+end;
+
+procedure TForm1.InitBuffer;
+begin
+     _Buffer[ 0 ] := TSingleM4.Translate( 0, 0, 0 );
+end;
+
+procedure TForm1.InitTextur;
+begin
+     _Textur.Imager.Grid.PoinsX := 4;
+
+     with _Textur.Imager.Grid.Map do
+     begin
+          Items[ 0 ] := TAlphaColorF.Create( 0, 0, 0 );
+          Items[ 1 ] := TAlphaColorF.Create( 0, 1, 0 );
+          Items[ 2 ] := TAlphaColorF.Create( 1, 1, 0 );
+          Items[ 3 ] := TAlphaColorF.Create( 1, 1, 1 );
+
+          DisposeOf;
+     end;
+end;
+
+procedure TForm1.InitImager;
+begin
+     _Imager.Grid.CellsX := _ImageW;
+     _Imager.Grid.CellsY := _ImageH;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.Draw;
+begin
+     //_Comput.RunARB;
+     _Comput.Run;
+
+     _Imager.CopyTo( Image1.Bitmap );
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     _Comput := TGLComput.Create;
+     _Buffer := TGLStoBuf<TSingleM4>.Create( GL_STATIC_DRAW );
+     _Textur := TGLPoiTex1D_TAlphaColorF.Create;
+     _Imager := TGLCelIma2D_TAlphaColorF.Create;
+
+     _ImageW := 600;
+     _ImageH := 600;
+
+     InitComput;
+     InitBuffer;
+     InitTextur;
+     InitImager;
+
+     Draw;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+     _Comput.DisposeOf;
+     _Buffer.DisposeOf;
+     _Textur.DisposeOf;
+     _Imager.DisposeOf;
+end;
+
+end. //######################################################################### ■
---
+++ _DATA/Comput.glsl
@@ -0,0 +1,107 @@
+﻿#version 430
+
+//#extension GL_ARB_compute_variable_group_size : enable
+
+//layout( local_size_variable ) in;
+layout( local_size_x = 10,
+        local_size_y = 10,
+        local_size_z =  1 ) in;
+
+////////////////////////////////////////////////////////////////////////////////
+
+const uvec3 _WorkGrupsN = gl_NumWorkGroups;
+
+//const uvec3 _WorkItemsN = gl_LocalGroupSizeARB;
+const uvec3 _WorkItemsN = gl_WorkGroupSize;
+
+const uvec3 _WorksN = _WorkGrupsN * _WorkItemsN;
+
+//############################################################################## ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【外部変数】
+
+layout( std430 ) buffer TBuffer
+{
+    mat4 _Matrix;
+};
+
+writeonly uniform image2D _Imager;
+
+uniform sampler1D _Textur;
+
+//############################################################################## ■
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TRay
+
+struct TRay
+{
+    vec3 Pos;
+    vec3 Vec;
+};
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THit
+
+struct THit
+{
+    float t;
+    vec3  Pos;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool HitShpere( TRay Ray, inout THit Hit )
+{
+    float B = dot( Ray.Pos, Ray.Vec );
+
+    float C = dot( Ray.Pos, Ray.Pos ) - 1;
+
+    if ( C > 0.0f && B > 0.0f ) return false;
+
+    float D = B * B - C;
+
+    if ( D < 0.0 ) return false;
+
+    float t = -B - sqrt( D );
+
+    if ( t < 0.0 ) return false;
+
+    Hit.t   = t;
+    Hit.Pos = Ray.Pos + t * Ray.Vec;
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void main()
+{
+  ivec2 I = ivec2( gl_GlobalInvocationID.xy );
+
+  vec3 EyePos = vec3( 0, 0, 2 );
+
+  vec3 ScreenPos;
+  ScreenPos.x = 2.0 * I.x / _WorksN.x - 1.0;
+  ScreenPos.y = 1.0 - 2.0 * I.y / _WorksN.y;
+  ScreenPos.z = 1.0;
+
+  TRay Ray;
+  Ray.Pos = vec3( 0, 0, 2 );
+  Ray.Vec = normalize( ScreenPos - EyePos );
+
+  THit Hit;
+  Hit.t = 10000;
+
+  vec4 C;
+  if ( HitShpere( Ray, Hit ) )
+  {
+    C = vec4( 1, 0, 0, 1 );
+  }
+  else
+  {
+    C = vec4( 0, 0, 0, 1 );
+  }
+
+  imageStore( _Imager, I, C );
+}
+
+//############################################################################## ■
